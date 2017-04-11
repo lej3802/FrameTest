c863d18bfe40861a7b5a841e08dfbc6b85e0e7d4
lej3802 <daylock3802@gmail.com>
2017-04-11 20:20:45 +0900

    #1 Make to ActivityLayout

---
+++ app/src/main/java/ljhi0222/kr/hs/emirim/framelayouttest/MainActivity.java
@@ -0,0 +1,17 @@
+package ljhi0222.kr.hs.emirim.framelayouttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button but1, but2, but3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
